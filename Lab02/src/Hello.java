41ccb23e4f46d10110eac736e642222b2acf9cba
Kaleb Bello <kalebbello@csus.edu>
2018-10-30 14:02:18 -0700

    Changed Tristan's name to lastname to test conflicts

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("218938422 - This line was added by Tristan Thibado.");
+		System.out.println("218938422 - This line was added by Tristan lastname.");
 		System.out.println("219011378 - This line was added by Jacob Hua.");
 		System.out.println("219242687 - This line was added by Kaleb Bello.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
